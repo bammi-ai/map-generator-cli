d9ff1eab008f9556c35caf0787d4bd7fcfcd1083
Juyeong Maing <mjy9088@naver.com>
2023-10-17 14:09:23 +0900

    change usage

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,51 @@
 extern crate bammi_ai_map_generator;
 
-use std::io;
+use std::env;
+use std::process;
+
+struct Config {
+    width: usize,
+    height: usize,
+    is_repeated: bool,
+    probability: f64,
+}
+
+impl Config {
+    fn new(args: &Vec<String>) -> Result<Config, String> {
+        if args.len() != 5 {
+            return Err("Must pass 4 arguments".to_string());
+        }
+        let width = args[1].parse().unwrap();
+        let height = args[2].parse().unwrap();
+        let is_repeated = args[3].parse().unwrap();
+        let probability = args[4].parse().unwrap();
+        Ok(Config {
+            width,
+            height,
+            is_repeated,
+            probability,
+        })
+    }
+}
 
 fn main() {
-    let mut input = String::new();
-    println!("Enter width:");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let width: usize = input.trim().parse().expect("Invalid input");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|_| {
+        println!("usage: {} width height is_repeated probability", args[0]);
+        process::exit(0);
+    });
 
-    input.clear();
-    println!("Enter height:");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let height: usize = input.trim().parse().expect("Invalid input");
+    let map = bammi_ai_map_generator::Map::new(
+        config.width,
+        config.height,
+        config.is_repeated,
+        config.probability,
+    );
+    let (width, height, _) = map.size();
 
-    input.clear();
-    println!("Enter a bool value (true or false):");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let is_repeated: bool = input.trim().parse().expect("Invalid input");
-
-    input.clear();
-    println!("Enter wall probability:");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let p: f64 = input.trim().parse().expect("Invalid input");
-
-    let map = bammi_ai_map_generator::Map::new(width, height, is_repeated, p);
-
-    for i in 0..map.height {
-        for j in 0..map.width {
-            print!(" {}", map.map[i][j]);
+    for i in 0..height {
+        for j in 0..width {
+            print!(" {}", map.map()[i][j]);
         }
         println!();
     }
